9e3e32898dee84803855436018caab748efbc702
Mathy <65187882+MatheusAstrum@users.noreply.github.com>
2026-08-12 04:35:16 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import json
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Lilith iniciada")
+
+bot.run(TOKEN)
