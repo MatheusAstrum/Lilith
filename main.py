57e3eb8b612013a2cdd3038c0eb6b8ddcfd7683d
Mathy <65187882+MatheusAstrum@users.noreply.github.com>
2026-08-13 04:38:30 +0000

    Comandos slash
    Comandos slash

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+
+    await bot.tree.sync()
     print("Lilith iniciada")
+    
 
 async def main():
     async with bot:
